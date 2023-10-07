d5271e18f8123675ab789a6a30653c8c5509f98e
Leonid Lavrov <274852@gmail.com>
2023-10-07 14:49:42 +0300

    Lesson 3: adding zoom

--- app/src/main/java/com/example/terminal/data/ApiService.kt
+++ app/src/main/java/com/example/terminal/data/ApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=MNwGwLiB59jRLoq_6U1WtlREJD_0LRBK")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=MNwGwLiB59jRLoq_6U1WtlREJD_0LRBK")
     suspend fun loadBars(): Result
 }
